a5c11dbd7e4a06dd47490c4ba95bdd2b354dced5
AbdulRahman Badran <ambadran717@hotmail.com>
2024-06-02 01:23:43 +0300

    added OLED screen reporting

--- Programming/muscle_sensor.py
+++ Programming/muscle_sensor.py
@@ -1,21 +1,10 @@
 from ad7705 import ad
 import time
-from machine import Timer
+from machine import Timer, Pin, I2C
 from servo import Servo
+import ssd1306
+  
 
-
-'''
-holding tuples values of upper and lower bound
-
-Listed from lowest intensity to highest intensity
-'''
-class MuscleIntensity:
-    NONE = 0
-    LOW = 1
-    MEDIUM = 2
-    HIGH = 3
-muscle_intensities_bounds = [(0, 1000), (1000, 8000), (8000, 10000), (10000, 40000)]
-    
 class Movement:
     '''
     A specific muscle movement consists of a specific muscle contractions in a specific order with specific intervals between each muscle contraction. 
@@ -66,6 +55,10 @@ class Finger:
             self._servo.write(self.MAXIMUM_DEGREE)
 
 class HumanoidHand:
+
+    # ssd1306 display
+    display = ssd1306.SSD1306_I2C(128, 64, I2C(1, scl=Pin(27), sda=Pin(26)))
+ 
     def __init__(self, fingers: tuple[Finger]):
 
         if len(fingers) != 5:
@@ -93,25 +86,82 @@ class HumanoidHand:
         tests each individual fingers
         '''
         for finger_ind in range(5):
-            print(f"Testing finger {finger_ind}:")
+            MuscleSensorStatus.report_custom(f"Testing finger:{finger_ind}!")
             self.fingers[finger_ind].contraction_toggle()
             time.sleep(1)
             self.fingers[finger_ind].contraction_toggle()
             time.sleep(1)
 
 class MuscleSensorStatus:
-    PENDING_MUSCLE_ACTIVIITY ="PENDING_MUSCLE_ACTIVIITY" 
-    READING_ORDER_IN_PROGRESS ="READING_ORDER_IN_PROGRESS" 
-    MOVEMENT_DETECTED ="MOVEMENT_DETECTED" 
-    MOVEMENT_INVALID ="MOVEMENT_INVALID" 
+    PENDING_ACTIVIITY = "PENDING_ACTIVIITY" 
+    READ_IN_PROGRESS =  "READ_IN_PROGRESS" 
+    MOVEMENT_DETECTED = "MOVEMENT_DETECTED" 
+    MOVEMENT_INVALID =  "MOVEMENT_INVALID" 
     IDLE ="IDLE" 
 
+    @classmethod
+    def report_full(cls, muscle: MuscleSensor):
+        '''
+        prints full report in terminal and on ssd1306 screen
+        '''
+        print(f"{muscle.status}: Muscle Intensity: {muscle.muscle_intensities_order} @ time: {muscle.current_time} ", end=' \r')
+        HumanoidHand.display.fill(0)
+        HumanoidHand.display.text(muscle.status, 0, 0, 1)
+        HumanoidHand.display.text(str(muscle.muscle_intensities_order), 0, 12, 1)
+        HumanoidHand.display.text(f"time: {muscle.current_time}", 0, 24, 1)
+        HumanoidHand.display.show()
+
+        time.sleep_us(100)
+
+    @classmethod
+    def report_status(cls, muscle: MuscleSensor):
+        '''
+        print status in terminal and ssd1306
+        '''
+        print(muscle.status)
+        HumanoidHand.display.text(muscle.status, 0, 0, 1)
+        HumanoidHand.display.show()
+        time.sleep_us(100)
+
+    @classmethod
+    def report_custom(cls, string, clear_display:bool=True, clear_line: bool=False, line: int=0, ending: str='\n'):
+        '''
+        display custome message in oled and terminal
+        AFTER deleting the whole screen
+        '''
+        print(string, end=ending)
+
+        if clear_display:
+            HumanoidHand.display.fill(0)
+        if clear_line:
+            # very useful to simulate '\r' like functionality in terminal
+            HumanoidHand.display.fill_rect(72, line, 127, 20, 0)
+
+        #TODO: write an algorithm to split text into words then print the most words possible
+        #       in the 16 character wide space per line available
+        HumanoidHand.display.text(string, 0, line, 1)
+        HumanoidHand.display.show()
+        time.sleep_us(100)
+
+
+'''
+holding tuples values of upper and lower bound
+
+Listed from lowest intensity to highest intensity
+'''
+class MuscleIntensity:
+    NONE = 0
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+ 
 class MuscleSensor:
     '''
     Class to interface EMG Muscle Sensor V3
 
     the muscle measurements raw values range from 36 hundreds to 45 hundreds
     '''
+    muscle_intensities_bounds = [(0, 1000), (1000, 8000), (8000, 10000), (10000, 40000)]
     MAXIMUM_SAMINGLING_TIME = const(1000)  # maximum muscle intensity change period is 1000ms
     def __init__(self, ad, movements: list[Movement]):
         self.ad = ad  # the AD7705 object
@@ -141,57 +191,61 @@ class MuscleSensor:
         dummy_contracted_intensities = []  # the maximum contraction happens for a few ms so the values must be filtered before adding them to the original list
 
         for _ in range(CALIBRATION_TIMES):
-            print("Please start relax muscle for 3 seconds")
-            time.sleep(2)
+            MuscleSensorStatus.report_custom("Relax Muscle!")
+            time.sleep(1)
             deadline = time.ticks_add(time.ticks_ms(), 1000)
             while time.ticks_diff(deadline, time.ticks_ms()) > 0:
                 value = ad.readADResultRaw()
-                print(f"Reading: {value}", end=' \r')
+                MuscleSensorStatus.report_custom(f"Reading: {value}", clear_display=False, clear_line=True, line=12, ending=' \r')
                 relaxed_intensities.append(value)
+            MuscleSensorStatus.report_custom(f"Avg Relax: {sum(relaxed_intensities)/len(relaxed_intensities)}", clear_display=False, line=24, ending='\n\n')
+            time.sleep(1)
 
-            print(f"Avg Relaxed value: {sum(relaxed_intensities)/len(relaxed_intensities)}\n\n")
 
-            print("Please contract your muscle as much as possible")
-
+            MuscleSensorStatus.report_custom("Contract Muscle!")
+            time.sleep(1)
             deadline = time.ticks_add(time.ticks_ms(), 1000)
             while time.ticks_diff(deadline, time.ticks_ms()) > 0:
                 value = ad.readADResultRaw()
-                print(f"Reading: {value}", end=' \r')
+                MuscleSensorStatus.report_custom(f"Reading: {value}", clear_display=False, clear_line=True, line=12, ending=' \r')
                 dummy_contracted_intensities.append(value)
             contracted_intensities.append(max(dummy_contracted_intensities))
-
-            print(f"Maximum value read: {contracted_intensities[-1]}")
+            MuscleSensorStatus.report_custom(f"Maximum: {contracted_intensities[-1]}", clear_display=False, line=24, ending='\n\n')
+            time.sleep(1)
 
         relaxed_value = int(sum(relaxed_intensities) / len(relaxed_intensities))
         contracted_value = int(sum(contracted_intensities) / len(contracted_intensities))
 
-        print(f"\n\nFinal relaxed value: {relaxed_value}\nFinal contracted value: {contracted_value}\n\n")
+        MuscleSensorStatus.report_custom(f"Avg Relaxed:")
+        MuscleSensorStatus.report_custom(f"{relaxed_value}", clear_display=False, line=12)
+        MuscleSensorStatus.report_custom(f"Avg Contracted:", clear_display=False, line=24)
+        MuscleSensorStatus.report_custom(f"{contracted_value}", ending='\n\n', clear_display=False, line=36)
 
         divisions = contracted_value//10
         v1 = relaxed_value + divisions*1
         v2 = relaxed_value + divisions*4
         v3 = relaxed_value + divisions*6
         v4 = relaxed_value + divisions*10
-        global muscle_intensities_bounds
-        muscle_intensities_bounds = [(0, v1), (v1, v2), (v2, v3), (v3, v4)]
+        self.muscle_intensities_bounds = [(0, v1), (v1, v2), (v2, v3), (v3, v4)]
 
     def read_mucsle_intensity(self) -> MuscleIntensity:
         '''
         reading current AD value and translating it into a muscle intensity range value
         '''
         value = self.ad.readADResultRaw()
-        for ind, bound in enumerate(muscle_intensities_bounds):
+        for ind, bound in enumerate(self.muscle_intensities_bounds):
             if bound[0] <= value <= bound[1]:
                 return ind
 
     def detect_muscle_contraction(self) -> bool:
-        self.status = MuscleSensorStatus.PENDING_MUSCLE_ACTIVIITY  #TODO: make while loop internal
+        self.status = MuscleSensorStatus.PENDING_ACTIVIITY  #TODO: make while loop internal
         current_value = self.read_mucsle_intensity()
         if not current_value:
             return False
 
         else:
-            self.status = MuscleSensorStatus.READING_ORDER_IN_PROGRESS
+            self.status = MuscleSensorStatus.READ_IN_PROGRESS
+            self.muscle_intensities_order = []  # I moved to be cleared with every start (not every end) so that I can see the pattern detected at the end
             self.muscle_intensities_order.append(current_value)
             self.timer = Timer(period=MuscleSensor.MAXIMUM_SAMINGLING_TIME, mode=Timer.PERIODIC, callback=self.read_contraction_order)
             return True
@@ -221,9 +275,9 @@ class MuscleSensor:
         '''
         movement: Movement = self.muscle_movement_hashes.get(tuple(self.muscle_intensities_order), None)
 
-        self.current_time = 0
         self.last_muscle_intensities_order = self.muscle_intensities_order
-        self.muscle_intensities_order = []
+        # self.muscle_intensities_order = [] // I moved to be cleared with every start (not every end) so that I can see the pattern detected at the end
+        self.current_time = 0
 
         return movement
             
@@ -241,17 +295,17 @@ class MuscleSensor:
 humanoid_hand = HumanoidHand((
 # Finger( 0, Movement(                           
 Finger( 16, Movement(                           
-    ( MuscleIntensity.HIGH, MuscleIntensity.MEDIUM, MuscleIntensity.LOW ),
-    (         0,                      1,                      3 )) ),
+    ( MuscleIntensity.LOW, MuscleIntensity.NONE, MuscleIntensity.NONE ),
+    (         0,                      1,                      2 )) ),
 
 # Finger( 1, Movement(                           
 Finger( 17, Movement(                           
-    ( MuscleIntensity.LOW, MuscleIntensity.MEDIUM, MuscleIntensity.HIGH ),
-    (         0,                      2,                      3         )) ),
+    ( MuscleIntensity.LOW, MuscleIntensity.LOW, MuscleIntensity.NONE ),
+    (         0,                      1,                      2)) ),
 
 # Finger( 2, Movement(                           
 Finger( 19, Movement(                           
-    ( MuscleIntensity.MEDIUM, MuscleIntensity.MEDIUM, MuscleIntensity.MEDIUM),
+    ( MuscleIntensity.LOW, MuscleIntensity.NONE, MuscleIntensity.LOW),
     (         0,                      1,                      2)) ),
 
 # Finger( 3, Movement(                           
@@ -261,40 +315,43 @@ Finger( 18, Movement(
 
 # Finger( 5, Movement(                           
 Finger( 20, Movement(                           
-    ( MuscleIntensity.HIGH, MuscleIntensity.HIGH, MuscleIntensity.HIGH),
+    ( MuscleIntensity.MEDIUM, MuscleIntensity.NONE, MuscleIntensity.NONE),
     (         0,                      1,                      2)) )
 
 ))
 
 movements = humanoid_hand.movement_tuple()
-print(f"Defined Movements:\n")
+print(f"Defined Moves:")  # unfortionately can't display this and the 5 movement in the oled screen at once :(
 for ind, movement in enumerate(movements):
-    print(f"Movement{ind+1}: {movement.muscle_intensities_order}")
+    MuscleSensorStatus.report_custom(f"M{ind+1}: {movement.muscle_intensities_order}", clear_display=False, line=ind*12)
+
 print('\n')
 
 muscle = MuscleSensor(ad, movements)
 
 def read_contraction_and_execute():
     muscle.detect_muscle_contraction()
-    while muscle.status == MuscleSensorStatus.PENDING_MUSCLE_ACTIVIITY:
+    while muscle.status == MuscleSensorStatus.PENDING_ACTIVIITY:
         muscle.detect_muscle_contraction()
-        print(f"{muscle.status}: Muscle Intensity: {muscle.muscle_intensities_order} @ time: {muscle.current_time} ", end=' \r')
+        MuscleSensorStatus.report_full(muscle)
 
     print()
 
-    while muscle.status == MuscleSensorStatus.READING_ORDER_IN_PROGRESS:
-        print(f"{muscle.status}: Muscle Intensity: {muscle.muscle_intensities_order} @ time: {muscle.current_time} ", end=' \r')
+    while muscle.status == MuscleSensorStatus.READ_IN_PROGRESS:
+        MuscleSensorStatus.report_full(muscle)
 
-    print(f"{muscle.status}: Muscle Intensity: {muscle.last_muscle_intensities_order} @ time: {muscle.current_time} ", end=' \r')
+    # MuscleSensorStatus.report_full(muscle)
     print()
 
     if muscle.status == MuscleSensorStatus.MOVEMENT_DETECTED:
-        print(f"{muscle.status}: ", end='')  # printing before the method clears the status
+        MuscleSensorStatus.report_status(muscle)
+
         detected_movement_ind = muscle.get_detected_muscle_movement()
-        print(detected_movement_ind)
+
+        MuscleSensorStatus.report_custom(f"Index: {detected_movement_ind}", clear_display=False, line=36)
+
         humanoid_hand.fingers[detected_movement_ind].contraction_toggle()
 
     elif muscle.status == MuscleSensorStatus.MOVEMENT_INVALID:
-        print(f"{muscle.status}!")
-
+        MuscleSensorStatus.report_status(muscle)
 
